c31538f528db1916f865c683dc785eaf5dffaa1b
praveenv <praveenv@uci.edu>
2019-02-18 21:53:46 -0800

    producer modify

--- dockerfiles/kafka-producer/kafka-producer.py
+++ dockerfiles/kafka-producer/kafka-producer.py
@@ -1,4 +1,5 @@
 from kafka import KafkaProducer
+import time
 from time import sleep
 from analog_sensor import sensor_reading
 from anomaly_detection import check_anomaly
@@ -7,13 +8,13 @@ from anomaly_detection import check_anomaly
 
 def initialize():
     producer = KafkaProducer(bootstrap_servers=['128.195.53.171:9092'])
+    return producer
 
- if __name__ == '__main__':
-    initialize()
+if __name__ == '__main__':
+    producer = initialize()
     while True:
         analog_sensor_value = sensor_reading()
-        print(analog_sensor_value)
         anomaly_flag = check_anomaly(analog_sensor_value)
         print(anomaly_flag)
-        producer.send('test',anomaly_flag)
+        producer.send('test','True')
         time.sleep(10)   
