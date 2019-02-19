a7f70f40fbad5c0b83af8fc5ead2ea149e72a13a
Praveen Venkateswaran <praveenv@uci.edu>
2019-02-18 19:21:51 -0800

    basic stuff

--- dockerfiles/kafka-consumer/kafka-consumer.py
+++ dockerfiles/kafka-consumer/kafka-consumer.py
@@ -1,3 +1,8 @@
 from kafka import KafkaConsumer
 
-
+consumer = KafkaConsumer('test', # topic to subscribe to - TODO : Include multiple topics
+        bootstrap_servers=['128.195.53.171:9092'], # server to connect to
+        auto_offset_reset='earliest', # if consumer goes down reload from oldest 
+        enable_auto_commit=True,
+        group_id='analog_sensors'
+        )
